6e3e550bb5e370a0a5d6846f73c3dac6edf35fc1
Thys <neganexus@disroot.org>
2019-12-04 21:44:29 -0800

    added inital code and matplotlibcpp header-only library

---
+++ cgp.cpp
@@ -0,0 +1,23 @@
+#include "rand.h"
+#include "matplotlibcpp.h"
+
+#include <iostream>
+#include <cmath>
+#include <string.h>
+#include <vector> 
+#include <deque>
+#include <algorithm>
+#include <numeric> 
+#include <iomanip>
+
+using namespace std;
+
+void initPopulation();
+vector<int> evaluate(vector<int> individual);
+int fitness(vector<int> individual);
+void mutate(vector<int> individual);
+
+int main() {
+
+    return 0;
+}
