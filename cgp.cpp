2a6bd9e4729b889eef556fe89476b4d553766872
Thys <neganexus@disroot.org>
2019-12-05 01:30:15 -0800

    Added stdin of test data

--- cgp.cpp
+++ cgp.cpp
@@ -17,6 +17,34 @@ int fitness(vector<int> individual);
 void mutate(vector<int> individual);
 
 int main() {
+    int numInputs;
+    int numOutputs;
+    int width;
+    int length;
+    int arity;
+    
+    int numSamples;
+
+
+    cin >> numInputs;
+    cin >> numOutputs;
+    cin >> width;
+    cin >> length;
+    cin >> arity;
+
+    cin >> numSamples;
+
+    int inputs[numSamples][numInputs];
+    int outputs[numSamples][numOutputs];
+
+    for (int i = 0; i < numSamples; ++i) {
+        for (int j = 0; j < numInputs; ++j) {
+            cin >> inputs[i][j]; 
+        }
+        for (int j = 0; j < numOutputs; ++j) {
+            cin >> outputs[i][j];
+        }
+    }
 
     return 0;
 }
